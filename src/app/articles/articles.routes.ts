a8ba83a42a420253e12b38a71cdff14a9e12bf4f
Carlos Mercado (dds_mineria) <carloswashingtonmercado@gmail.com>
2026-07-10 18:18:50 -0300

    update # translation into english

--- src/app/articles/articles.routes.ts
+++ src/app/articles/articles.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { ArticleListComponent } from './article-list/article-list.component';
-import { ArticleDetailComponent } from './article-detail/article-detail.component';
+import { ArticleDetailComponent } from './article-details/article-details.component';
 
 export const ARTICLES_ROUTES: Routes = [
     { path: '', component: ArticleListComponent },
